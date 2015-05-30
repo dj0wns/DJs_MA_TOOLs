a91bbfdabe72b56840325e485adb029734021851
Jonathan Lindahl <jonathanjl99@gmail.com>
2015-05-30 20:05:48 +0200

    Added Materialize, ui, basic ui functionality.

---
+++ mstexplorer/js/ui.js
@@ -0,0 +1,9 @@
+function UI()
+{
+
+}
+
+// Static globals
+UI.ERROR_CLASS = "red darken-2"; // Classes to apply to error toasts.
+UI.TOAST_LONG = 3500; // Total ms to show long toasts.
+UI.TOAST_SHORT = 2000; // Total ms to show short toasts.
