b57a30de01ae1dfe7634fa68b24b8871a47d911b
dj0wns <derekjones@asu.edu>
2020-05-15 01:49:59 +0000

    Added prompts and file name changes + support for more files!

--- mod_insert.py
+++ mod_insert.py
@@ -3,8 +3,26 @@ import shutil
 import tempfile
 import os
 import argparse
+from distutils.util import strtobool
 
 import mst_extract, mst_insert, wld_extractor
+
+file_type_list =[
+  ".ape",
+  ".cam",
+  ".csv",
+  ".dds",
+  ".fnt",
+  ".fpr",
+  ".gt",
+  ".mtx",
+  ".new",
+  ".rdg",
+  ".rdx",
+  ".sfb",
+  ".sfb",
+  ".sma",
+  ".wvb"]
 
 def execute(mst, modfile):
   try:
@@ -26,21 +44,58 @@ def execute(mst, modfile):
       return
     with open(config_path, 'r') as file:
       f_to_open = file.readline().strip()
-    if f_to_open.endswith(".wld"):
-      try:
+    files_to_insert = []
+    try:
+      wld_tempdir = tempfile.mkdtemp()
+      if f_to_open.endswith(".wld"):
         #First extract the wld file
-        wld_tempdir = tempfile.mkdtemp()
         little_endian = mst_extract.extract(mst, wld_tempdir, False, f_to_open)
         #now insert shapes into the wld
         wld_file = os.path.join(wld_tempdir, f_to_open)
         wld_extractor.execute(not little_endian, False, False, True, False, wld_file, dirpath)
-        #now insert wld back into the mst
-        mst_insert.execute(not little_endian, mst, [wld_file], "")
-      except Exception as e:
-        print(e)
-      finally:
-        #make sure to clean up after ourselves
-        shutil.rmtree(wld_tempdir)
+        #prompt user if this is ok
+        while True:
+          try:
+            value = strtobool(input("By default this mod replaces " + f_to_open + ". Would you like to replace a different .wld instead? "))
+          except ValueError:
+            continue
+          if not value:
+            break
+          new_wld = input("Which .wld would you like to replace? ")
+          os.rename(wld_file, os.path.join(wld_tempdir, new_wld))
+          wld_file = os.path.join(wld_tempdir, new_wld)
+          break
+        files_to_insert.append(wld_file)
+      else:
+        little_endian = mst_extract.extract(mst, wld_tempdir, False, "NO_FILE")
+
+      #now get other non-json files in the mst and insert them
+      for f in os.listdir(dirpath):
+        name, extension = os.path.splitext(f)
+        if extension in file_type_list:
+          files_to_insert.append(os.path.join(dirpath,f))
+      #let the user know what is being inserted
+      print("This mod will replace the following files: ")
+      for f in files_to_insert:
+        filename = os.path.basename(f)
+        print(filename)
+      #prompt user if this is ok
+      while True:
+        try:
+          value = strtobool(input("Continue? "))
+        except ValueError:
+          continue
+        if not value:
+          return
+        break
+
+      #now insert wld back into the mst
+      mst_insert.execute(not little_endian, mst, files_to_insert, "")
+    except Exception as e:
+      print(e)
+    finally:
+      #make sure to clean up after ourselves
+      shutil.rmtree(wld_tempdir)
 
 
   except Exception as e:
