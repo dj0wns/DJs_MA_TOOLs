25eefe17ef8de20e0bc73231fe5c4a98e55a1342
Derek Jones <derekjones@asu.edu>
2020-05-23 01:22:38 -0400

    Added basic shape creation and file output to maze_gen

--- lib/ma_util.py
+++ lib/ma_util.py
@@ -2,6 +2,7 @@ import jsonpickle
 import json
 import math
 import struct
+from . import init_classes
 
 # for building elements without constructor - probably should have written my constructors different but it is what it is
 class Empty(object):
@@ -50,3 +51,94 @@ class Vector:
     raw += struct.pack(float_endian, self.y)
     raw += struct.pack(float_endian, self.z)
     return raw
+
+def default_init():
+  init = Empty()
+  init.__class__ = init_classes.InitObject
+  init.data = {}
+  init.data['offset'] = 0
+  init.data['shape_type'] = "FWORLD_SHAPETYPE_MESH"
+  init.data['shape_offset'] = -1
+  #default orientation
+  init.data['Right_X'] = 1.
+  init.data['Right_Y'] = 0.
+  init.data['Right_Z'] = 0.
+  init.data['Up_X'] = 0.
+  init.data['Up_Y'] = 1.
+  init.data['Up_Z'] = 0.
+  init.data['Front_X'] = 0.
+  init.data['Front_Y'] = 0.
+  init.data['Front_Z'] = 1.
+  init.data['Position_X'] = 0
+  init.data['Position_Y'] = 0
+  init.data['Position_Z'] = 0
+  init.data['shape_index'] = -1
+  init.data['pointer_to_game_data'] = -1
+  return init
+
+def default_mesh_shape():
+  shape = Empty()
+  shape.__class__ = init_classes.ShapeData
+  shape.data = {}
+  shape.shape_type = "FWORLD_SHAPETYPE_MESH"
+  return shape
+
+def default_point_shape():
+  shape = Empty()
+  shape.__class__ = init_classes.ShapeData
+  shape.data = {}
+  shape.shape_type = "FWORLD_SHAPETYPE_POINT"
+  shape.data['offset'] = -1
+  return shape
+
+def default_gamedata():
+  gamedata = Empty()
+  gamedata.__class__ = init_classes.GameDataHeader
+  gamedata.data = {}
+  gamedata.data['offset'] = -1
+  gamedata.data['size'] = -1
+  gamedata.data['number_of_tables'] = 0
+  gamedata.data['pointer_to_tables'] = 0
+  gamedata.data['flags'] = 0
+  gamedata.tables=[]
+  return gamedata
+
+def add_table_to_gamedata(gamedata, table_name, values, var_type):
+  table = Empty()
+  table.__class__ = init_classes.GameDataTable
+  table.data = {}
+  table.fields = []
+  table.data['offset'] = -1
+  table.data['header_offset'] = -1
+  table.data['keystring_pointer'] = -1
+  table.data['keystring_length'] = len(table_name)
+  table.data['num_fields'] = len(values)
+  table.data['table_index'] = -1
+  table.data['field_offset'] = -1
+  table.data['keystring'] = table_name.encode('ascii')
+  #now add fields
+  for value in values:
+    field = Empty()
+    field.__class__ = init_classes.GameDataField
+    field.data = {}
+    field.data['offset'] = -1
+    field.data['data_type'] = var_type
+    if var_type == "FLOAT":
+      field.data['float'] = value
+      field.data['string_length'] = 0
+    elif var_type == "STRING":
+      field.data['string_pointer'] = -1
+      field.data['string_length'] = len(value)
+      field.data['string'] = value.encode('ascii')
+    elif var_type == "WIDESTRING":
+      field.data['widestring_pointer'] = -1
+      field.data['string_length'] = len(value)
+      field.data['widestring'] = value.encode('utf-8')
+    else:
+      print ('INVALID var type')
+      return None
+    table.fields.append(field)
+  gamedata.tables.append(table)
+
+
+  
--- tools/generate_maze.py
+++ tools/generate_maze.py
@@ -1,12 +1,14 @@
 import argparse
 import os
+from maze_lib import graph_to_shapes, output_shapes
 
 fpath=os.path.realpath(__file__)
 py_path=os.path.dirname(fpath)
 
 
-def execute(length, width, center_x, center_y, tesselation, mesh_name, mesh_width, path_thickness, routing, output):
+def execute(length, width, center_x, center_z, tesselation, mesh_name, mesh_width, path_thickness, routing, output):
   init_shape_game_data_list = {} 
+  maze_dict = {}
   if mesh_name is None:
     mesh_name = "goshcrate02"
   if mesh_width is None:
@@ -24,14 +26,16 @@ def execute(length, width, center_x, center_y, tesselation, mesh_name, mesh_widt
     #   call generate_walls_and_entities()
     #     params:  maze graph, center_x, center_y, rotation, difficulty
     #     returns: list of shapes
+    init_shape_game_data_list = graph_to_shapes.graph_to_shapes(maze_dict, mesh_name, center_x, center_z)
     # TODO
     #   call generate_shapes (in lib/ma_util.py) given list of shapes and output
+    output_shapes.output_shapes(init_shape_game_data_list, output)
 
   else:
     print ("Unrecognized maze tesselation type")
     return False
 
-  #output to folder
+
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description="Generate a maze")
@@ -40,7 +44,7 @@ if __name__ == '__main__':
   routing_style.add_argument("-b", "--braid", help="A maze with no dead ends - it loops back on itself", action='store_const', dest="routing", const='braid')
   routing_style.add_argument("-u", "--unicursal", help="A maze with exactly 1 path", action='store_const', dest="routing", const='unicursal')
   parser.add_argument("-x", "--center-x", help="x-value of the maze center", default=0)
-  parser.add_argument("-y", "--center-y", help="x-value of the maze center", default=0)
+  parser.add_argument("-z", "--center-z", help="z-value of the maze center", default=0)
   parser.add_argument("-t", "--tesselation", help="orthogonal: a simple rectangular maze\nOther maze types to come!", default="orthogonal")
   expertArgs = parser.add_argument_group('expert arguments - don\'t use these unless you know what you are doing')
   expertArgs.add_argument("--mesh-name", help="Name of mesh you want to use, using the default mesh is recommended")
@@ -51,4 +55,4 @@ if __name__ == '__main__':
   requiredNamed.add_argument("-w", "--width", help="Width of Maze in game units", required=True)
   parser.add_argument("output", type=str, help="Output folder")
   args = parser.parse_args()
-  execute(args.length, args.width, args.center_x, args.center_y, args.tesselation, args.routing, args.mesh_name, args.mesh_width, args.path_thickness, args.output)
+  execute(args.length, args.width, args.center_x, args.center_z, args.tesselation, args.routing, args.mesh_name, args.mesh_width, args.path_thickness, args.output)
